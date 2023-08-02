78626fe11a309e7a718fceb7c18b45ea4d28291b
LaughDieMeh <140854916+LaughDieMeh@users.noreply.github.com>
2023-08-02 23:01:15 +0800

    Update Boat.java
    changed to .equals

--- Boat.java
+++ Boat.java
@@ -17,54 +17,54 @@ public class Boat {
         int initialPosition = position;
         position +=roll;
         if (position > 198) {
-			position = 198;
-		} 
+            position = 198;
+        }
         System.out.printf("\nYou rolled a %d ! ", roll / 2);
-       
+
         // still thinking how to represent names System.out.printf("Player %s, %s has won!!\n", boat, playername);
 
-    	if (River.getRiver(initialPosition) == boat) {
-    		River.setRiver(initialPosition, " ");
-    	} else {
-    		River.setRiver(initialPosition, opp);
-    	}
-    	
-    	System.out.println();
+        if (River.getRiver(initialPosition).equals(boat)) {
+            River.setRiver(initialPosition, " ");
+        } else {
+            River.setRiver(initialPosition, opp);
+        }
 
-    	do { 
-        	if (River.getRiver(position) == "C") {
+        System.out.println();
+
+        do {
+            if (River.getRiver(position).equals("C")) {
                 int forwardpush = Current.PushedForwardCurrents();
-                position += forwardpush;   
+                position += forwardpush;
                 if (position > 198) {
-        			position = 198;
-        		} 
+                    position = 198;
+                }
                 System.out.println("AND YOU HAVE BEEN PUSHED FORWARD BY A CURRENT (+" + forwardpush / 2 + ") !!");
-                
-        	} else if (River.getRiver(position) == "#") {
-        		int backwardpush = Trap.PushedBackwardTraps();
-        		position -= backwardpush;
-        		if (position < 0) {
-        			position = 0;
-        		}
-        		System.out.println("BUT YOU HAVE BEEN PUSHED BACK BY A TRAP (-" + backwardpush / 2 + ") !!");
-        		
-        	} else {} 
-    	} while ( River.getRiver(position) == "C" || River.getRiver(position) == "#" );
+
+            } else if (River.getRiver(position).equals("#")) {
+                int backwardpush = Trap.PushedBackwardTraps();
+                position -= backwardpush;
+                if (position < 0) {
+                    position = 0;
+                }
+                System.out.println("BUT YOU HAVE BEEN PUSHED BACK BY A TRAP (-" + backwardpush / 2 + ") !!");
+
+            } else {}
+        } while ( River.getRiver(position).equals("C") || River.getRiver(position).equals("#") );
 
 
-    	placement(position, boat, opp);
+        placement(position, boat, opp);
         River.printRiver();
         return position;
     }
 
     public static void placement(int position, String boat, String opp) {
-        if (River.getRiver(position) == opp) {
-        	River.setRiver(position, "½");
-        	System.out.println("Boat " + boat + " is at box " + ((position / 2) + 1) + " as well!!");
-        	
-        } else if (River.getRiver(position) == " ") {
-        	River.setRiver(position, boat);
-        	System.out.println("Boat " + boat + " is at box " + ((position / 2) + 1) + " !!\n");
+        if (River.getRiver(position).equals(opp)) {
+            River.setRiver(position, "½");
+            System.out.println("Boat " + boat + " is at box " + ((position / 2) + 1) + " as well!!");
+
+        } else if (River.getRiver(position).equals(" ")) {
+            River.setRiver(position, boat);
+            System.out.println("Boat " + boat + " is at box " + ((position / 2) + 1) + " !!\n");
         } else {
         }
     }
